04465bc82ddb04af7b5b7768794cb484feb71a85
Rudraksh <rudrakshkulkarni4@gmail.com>
2026-05-21 15:56:37 +0530

    fix render port issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
